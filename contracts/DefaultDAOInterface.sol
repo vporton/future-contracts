5907c69f41c7165416f1f93da127cb87faaf5929
Victor Porton <porton@narod.ru>
2021-01-05 10:26:03 +0200

    security vulnerability fix

--- contracts/DefaultDAOInterface.sol
+++ contracts/DefaultDAOInterface.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.7.1;
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./DAOInterface.sol";
 
-contract DefaultDAOInterface is DAOInterface {
+contract DefaultDAOInterface is DAOInterface, Ownable {
     function checkPersonDead(address /*account*/) external pure override { }
 
     function checkAllowedRestoreAccount(address /*oldAccount_*/, address /*newAccount_*/) external pure override {
