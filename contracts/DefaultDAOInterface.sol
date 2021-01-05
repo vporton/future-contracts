99c673aed8c3b1e1d3e80beb6aa1b2e4bb9c8118
Victor Porton <porton@narod.ru>
2021-01-05 10:26:36 +0200

    Revert "security vulnerability fix"
    This reverts commit 5907c69f41c7165416f1f93da127cb87faaf5929.

--- contracts/DefaultDAOInterface.sol
+++ contracts/DefaultDAOInterface.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.7.1;
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./DAOInterface.sol";
 
-contract DefaultDAOInterface is DAOInterface, Ownable {
+contract DefaultDAOInterface is DAOInterface {
     function checkPersonDead(address /*account*/) external pure override { }
 
     function checkAllowedRestoreAccount(address /*oldAccount_*/, address /*newAccount_*/) external pure override {
