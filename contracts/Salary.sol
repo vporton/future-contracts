3bdc12844c56126b76d49ba8c3e8f21ca5633463
Victor Porton <porton@narod.ru>
2021-01-10 07:17:41 +0200

    comment

--- contracts/Salary.sol
+++ contracts/Salary.sol
@@ -2,7 +2,17 @@
 pragma solidity ^0.7.1;
 import "./BaseBidOnAddresses.sol";
 
-/// TODO: Allow the DAO to addjust registration date to pay salary retrospectively?
+/// TODO: Allow the DAO to adjust registration date to pay salary retrospectively?
+/// It would cause this effect: A scientist who is already great may register then his date is moved back
+/// in time and instantly he or she receives a very big sum of money to his account.
+/// If it is done erroneously, there may be no way to move the registration date again forward in time,
+/// because the tokens may be already withdrawn. And it cannot be done in a fully decentralized way because
+/// it needs oracles. So errors are seem inevitable. If there may be errors, maybe better not to allow it at all?
+/// On the other hand, somebody malicious may create and register in my system a pool of Ethereum addresses that
+// individuals can receive from them as if they themselves registered in the past.
+/// So it in some cases (if the registration date is past the contract deployment) this issue is impossible to
+/// mitigate.
+/// But should we decide what to disallow to the global voters?
 contract Salary is BaseBidOnAddresses {
     event CustomerRegistered(
         address customer,
