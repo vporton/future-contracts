6edd74fed67b43adf5537b5d12383cefe3d59ba3
Victor Porton <porton@narod.ru>
2021-01-15 21:57:03 +0200

    doc comments

--- contracts/BaseBaseLock.sol
+++ contracts/BaseBaseLock.sol
@@ -6,11 +6,12 @@ import { ERC1155WithMappedAddressesAndTotals } from "./ERC1155/ERC1155WithMapped
 import { IERC1155TokenReceiver } from "./ERC1155/IERC1155TokenReceiver.sol";
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
+/// A base class to lock collaterals and distribute them proportional to an oracle result.
 abstract contract BaseBaseLock is ERC1155WithMappedAddressesAndTotals, IERC1155TokenReceiver {
     using ABDKMath64x64 for int128;
     using SafeMath for uint256;
 
-    // Condditional tokens vs collaterals.
+    /// Conditional tokens vs collaterals.
     enum TokenKind { TOKEN_CONDITIONAL, TOKEN_DONATED }
 
     /// Emitted when an oracle is created.
