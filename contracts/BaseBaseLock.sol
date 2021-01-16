3399cdc7b157e8ba73dc02bc0855cab497c2869e
Victor Porton <porton@narod.ru>
2021-01-16 09:34:02 +0200

    comment

--- contracts/BaseBaseLock.sol
+++ contracts/BaseBaseLock.sol
@@ -7,6 +7,9 @@ import { IERC1155TokenReceiver } from "./ERC1155/IERC1155TokenReceiver.sol";
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 /// A base class to lock collaterals and distribute them proportional to an oracle result.
+///
+/// TODO: Don't use account addresses as conditional IDs and add ability to move conditionals to other accounts?
+///       BTW, could this replace the current account restoration mechanism?
 abstract contract BaseBaseLock is ERC1155WithTotals , IERC1155TokenReceiver {
     using ABDKMath64x64 for int128;
     using SafeMath for uint256;
