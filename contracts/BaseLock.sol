4b766182fb0617275f37e374201ed83fcbc63c9f
Victor Porton <porton@narod.ru>
2021-01-17 23:35:52 +0200

    removed an impossible TODO

--- contracts/BaseLock.sol
+++ contracts/BaseLock.sol
@@ -10,7 +10,6 @@ import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 ///
 /// One can also donate/bequest a smart wallet (explain how).
 ///
-/// TODO: Ability to split/join conditionals?
 /// TODO: Use variables like `_x` as arguments.
 ///
 /// Inheriting from here don't forget to create `createOracle()` external method.
