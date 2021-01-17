67ba8d88bc43081e2942d384b155853490bf05d5
Victor Porton <porton@narod.ru>
2021-01-17 18:04:19 +0200

    TODO comment

--- contracts/SalaryWithDAO.sol
+++ contracts/SalaryWithDAO.sol
@@ -87,6 +87,7 @@ contract SalaryWithDAO is BaseRestorableSalary {
         super._mintToCustomer(customer, conditionalTokenId, amount, data);
     }
 
+    // TODO: Should be called directly by the DAO or by anyone who passes a check by the DAO?
     function forciblyRecalculateSalary(uint256 condition, address account) public onlyDAO {
         // TODO: Check that `minAllowedRecreate` seconds passed.
         _recreateCondition(condition);
