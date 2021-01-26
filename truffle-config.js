6c15398e593837de8d2b7e161ad774722a0f6faa
Victor Porton <porton@narod.ru>
2021-01-26 22:24:20 +0200

    deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,24 +1,75 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 const config = {
   networks: {
     mainnet: {
+      provider: function() {
+        return new HDWalletProvider({
+          privateKeys: [process.env.TESTNET_PRIVATE_KEY],
+          providerOrUrl:
+            "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY
+        });
+      },
       host: "localhost",
       port: 8545,
       network_id: "1"
     },
     ropsten: {
+      provider: function() {
+        return new HDWalletProvider({
+          privateKeys: [process.env.TESTNET_PRIVATE_KEY],
+          providerOrUrl:
+            "https://ropsten.infura.io/v3/" + process.env.INFURA_KEY
+        });
+      },
       host: "localhost",
       port: 8545,
       network_id: "3"
     },
     kovan: {
+      provider: function() {
+        return new HDWalletProvider({
+          privateKeys: [process.env.TESTNET_PRIVATE_KEY],
+          providerOrUrl: "https://kovan.infura.io/v3/" + process.env.INFURA_KEY
+        });
+      },
       host: "localhost",
       port: 8545,
       network_id: "42"
     },
     rinkeby: {
+      provider: function() {
+        return new HDWalletProvider({
+          privateKeys: [process.env.TESTNET_PRIVATE_KEY],
+          providerOrUrl:
+            "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY
+        });
+      },
       host: "localhost",
       port: 8545,
       network_id: "4"
+    },
+    bsc: {
+      provider: function() {
+        return new HDWalletProvider({
+          privateKeys: [process.env.TESTNET_PRIVATE_KEY],
+          providerOrUrl: `https://bsc-dataseed1.binance.org`
+        });
+      },
+      host: "localhost",
+      port: 8545,
+      network_id: "56"
+    },
+    bsctestnet: {
+      provider: function() {
+        return new HDWalletProvider({
+          privateKeys: [process.env.TESTNET_PRIVATE_KEY],
+          providerOrUrl: `https://data-seed-prebsc-1-s1.binance.org:8545`
+        });
+      },
+      host: "localhost",
+      port: 8545,
+      network_id: "97"
     },
     xdai: {
       host: "localhost",
