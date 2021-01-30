fb32cef827b5e753d30cbbc5a8e787547c747215
Victor Porton <porton@narod.ru>
2021-01-30 20:49:19 +0200

    Aggregator.sol finished (not tested) except of ERC-721

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.7.1;
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-// import { BequestModule } from "@vporton/safe-bequest-module/contracts/BequestModule.sol";
 import { BaseLock } from "./BaseLock.sol";
 
 contract Enum {
@@ -24,17 +24,27 @@ interface IBequestModule {
 }
 
 contract Aggregator {
+    using SafeMath for uint256;
+
     BaseLock locker;
     IBequestModule bequest;
+    IERC1155 erc20Wrapper;
 
-    constructor(BaseLock _locker, IBequestModule _bequest) {
+    mapping(uint64 => uint256) public oracleBalances;
+
+    constructor(BaseLock _locker, IBequestModule _bequest, IERC1155 _erc20Wrapper) {
         locker = _locker;
         bequest = _bequest;
+        erc20Wrapper = _erc20Wrapper;
+        _erc20Wrapper.setApprovalForAll(address(_erc20Wrapper), true);
     }
 
     /// Can be called by anybody.
-    function takeDonationERC1155(ModuleManager _safe, IERC1155 _erc1155Contract, uint256 _tokenId, bytes calldata data) public {
+    function takeDonationERC1155(uint64 _oracleId, ModuleManager _safe, IERC1155 _erc1155Contract, uint256 _tokenId, bytes memory data)
+        public
+    {
         uint256 _amount = _erc1155Contract.balanceOf(address(_safe), _tokenId);
+        oracleBalances[_oracleId] = oracleBalances[_oracleId].add(_amount);
         bytes memory txData = abi.encodeWithSelector(
             bytes4(keccak256("safeTransferFrom(address,address,uint256,uint256,bytes)")),
             address(_safe), address(this), _tokenId, _amount, data);
@@ -42,25 +52,17 @@ contract Aggregator {
     }
 
     /// Can be called by anybody.
-    ///
-    /// FIXME: Can the Locker take ERC-20?
-    function takeDonationERC20(ModuleManager _safe, IERC20 _erc20Contract) public {
-        uint256 _amount = _erc20Contract.balanceOf(address(_safe));
-        bytes memory txData = abi.encodeWithSelector(
-            bytes4(keccak256("transfer(address,uint256)")),
-            address(this), _amount);
-        bytes memory resultData = bequest.executeReturnData(_safe, address(_erc20Contract), 0, txData, Enum.Operation.CALL);
-        bool result = resultData[0] != 0; // may use non-optimal gas
-        require(result, "Transfer failed");
+    function takeDonationERC20(uint64 _oracleId, ModuleManager _safe, IERC20 _erc20Contract) public {
+        bytes memory _data;
+        takeDonationERC1155(_oracleId, _safe, erc20Wrapper, uint256(address(_erc20Contract)), _data);
     }
 
     // TODO: ERC-721
 
     /// Can be called by anybody.
-    ///
-    /// FIXME: We should not send to an arbitrary oracle!
-    function sendDonation(IERC1155 _contractAddress, uint256 _tokenId, uint64 _oracleId, bytes calldata _data) public {
+    function sendDonation(uint64 _oracleId, IERC1155 _contractAddress, uint256 _tokenId, bytes calldata _data) public {
         uint256 _amount = _contractAddress.balanceOf(address(this), _tokenId);
+        oracleBalances[_oracleId] = oracleBalances[_oracleId].sub(_amount); // reverts on underflow
         locker.donate(
             _contractAddress,
             _tokenId,
