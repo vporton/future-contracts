5db3ddc0fb0f2a612b4df04fc9c0e9276dc3b32d
Victor Porton <porton@narod.ru>
2021-01-30 23:45:03 +0200

    bug fix

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -4,6 +4,8 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import { ERC1155Holder } from "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
+import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
 import { BaseLock } from "./BaseLock.sol";
 
 contract Enum {
@@ -23,8 +25,8 @@ interface IBequestModule {
         external returns (bytes memory returnData);
 }
 
-// FIXME: 1155 and 721 receiver.
-contract Aggregator {
+// FIXME: One this contract for several lockers.
+contract Aggregator is ERC721Holder, ERC1155Holder {
     using SafeMath for uint256;
 
     BaseLock locker;
