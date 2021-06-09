fd0838d807402289286d52d7f282d87c628901f4
Victor Porton <porton@narod.ru>
2021-06-10 02:18:42 +0300

    bug fix

--- contracts/NFTRestoreContract.sol
+++ contracts/NFTRestoreContract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.7.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @notice Account restoration contract controlled by NFT.
 /// @author Victor Porton
