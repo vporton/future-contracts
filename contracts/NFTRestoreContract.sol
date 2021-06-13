f9ce8de1c46ad1329172ec3fc37b5c2d0b9585f4
Victor Porton <porton@narod.ru>
2021-06-13 16:18:53 +0300

    FIXME-related things

--- contracts/NFTRestoreContract.sol
+++ contracts/NFTRestoreContract.sol
@@ -6,12 +6,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @notice Account restoration contract controlled by NFT.
 /// @author Victor Porton
+/// FIXME: Combine both NFTs to one ERC-1155 for better interoperability?
 contract NFTRestoreContract is ERC721, Ownable {
     constructor() ERC721("Right to control your salary.", "TakeMySalary") { }
 
     function mint(address _account, uint256 _tokenId) public virtual onlyOwner {
         _mint(_account, _tokenId);
     }
+
+    // function burn(uint256 tokenId) public virtual {
+    //     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+    //     _burn(tokenId);
+    // }
 
     // FIXME: It's wrong: If the condition owner transfers his NFTSalaryRecipient but not his NFTRestoreContract to another person, technology
     // then he would not be able to restore lost funds this way. Need to modify the logic.
