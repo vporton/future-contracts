2f3126025953075cafa3dc33ec7b5472b598a183
Victor Porton <porton@narod.ru>
2021-06-14 05:48:10 +0300

    mnt/burn NFTs

--- contracts/NFTRestoreContract.sol
+++ contracts/NFTRestoreContract.sol
@@ -20,17 +20,16 @@ contract NFTRestoreContract is ERC721 {
     }
 
     function mint(address _account, uint256 _tokenId) public virtual onlyRecipients {
+        require(recipients.ownerOf(_tokenId) == _account, "You are not owner.");
         _mint(_account, _tokenId);
     }
 
-    // function burn(uint256 tokenId) public virtual {
-    //     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-    //     _burn(tokenId);
-    // }
+    /// It's useful for transfers of the recipient token, to avoid transferring both tokens.
+    function burn(uint256 tokenId) public virtual {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+        _burn(tokenId);
+    }
 
-    // FIXME: It's wrong: If the condition owner transfers his NFTSalaryRecipient but not his NFTRestoreContract to another person, technology
-    // then he would not be able to restore lost funds this way. Need to modify the logic.
-    // Possible solution: be able to burn notary token (by the notary) and mint it again (by the recipient).
     function checkRestoreRight(address _origOldAccount) public view {
         require(ownerOf(uint256(_origOldAccount)) == msg.sender, "No restore right.");
     }
